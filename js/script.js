56cbddb3b7bdc741471819fdce6f2a7f09d72686
Sandra Szewczyk <sandraszewczyk.zg@gmail.com>
2021-05-14 14:08:34 +0200

    changing the type of calculator

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
+
 let formElement = document.querySelector(".js-calculator");
-let exchangeElement = document.querySelector(".js-exchange");
-let eurElement = document.querySelector(".js-eur");
 let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.54
+let rateGBP = 5.27
+let rateUSD = 3.75
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let exchange = exchangeElement.value;
-    let amount = amountElement.value;
-    eurElement.value = (exchange * amount).toFixed(2);
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+let result;
 
 
+switch (currency) {
+case "EUR":
+result = amount / rateEUR;
+break;
+case "GBP":
+    result = amount / rateGBP;
+    break;
+case "USD":
+    result = amount / rateUSD;
+    break;
+
+}
+resultElement.value = result.toFixed(2);
 
 });
+
